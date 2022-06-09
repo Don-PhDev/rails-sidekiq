98d15cf49e7fc37f3f6c5427004a8625da076190
Don Forrest <forrest@philippinedev.com>
2022-06-09 21:43:33 +0800

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   get "welcome/index"
 
+  # route where any visitor require the helloWorldJob to be triggered
+  post "welcome/trigger_job"
+
+  # where visitor are redirected once job has been called
+  get "other/job_done"
+
   root to: "welcome#index"
 end
