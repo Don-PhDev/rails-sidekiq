8d0c51daaebdced53612d5eb14dd4489c29473f0
Don Forrest <forrest@philippinedev.com>
2022-06-09 21:43:22 +0800

    Create hello_world_job

---
+++ app/jobs/hello_world_job.rb
@@ -0,0 +1,10 @@
+class HelloWorldJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Simulates a long, time-consuming task
+    sleep 5
+    # Will display current time, milliseconds included
+    p "hello from HelloWorldJob #{Time.now().strftime('%F - %H:%M:%S.%L')}"
+  end
+end
