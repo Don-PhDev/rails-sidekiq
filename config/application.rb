4009a669d42403e8dd8358b4639b1772176be3a7
Don Forrest <forrest@philippinedev.com>
2022-06-09 21:40:57 +0800

    Use Asia/Singapore as time_zone

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,7 @@ module Sidekiqrails
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Asia/Singapore"
     # config.eager_load_paths << Rails.root.join("extras")
 
     # Don't generate system test files.
